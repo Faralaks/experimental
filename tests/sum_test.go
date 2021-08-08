9aec0bb6e537338364d76a0476b46b6a34e86b21
Faralaks <Faralaks@ya.ru>
2021-08-08 13:17:02 +0300

    Проверка автокопирайта

--- tests/sum_test.go
+++ tests/sum_test.go
@@ -1,3 +1,8 @@
+/*~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+~ Most likely it was written by Faralaks!
+~ Use this code for your own purposes, if they do not contradict the license installed on the project.
+~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~*/
+
 package tests
 
 import (
@@ -14,7 +19,7 @@ func TestSum(t *testing.T) {
 
 	require.Equal(t, 1, s.X)
 	require.Equal(t, 2, s.Y)
-	require.Equal(t, 3, s.Z)
+	require.Equal(t, 3, s.Z) // Почининая строка
 
 	require.True(t, s.AfterCalc)
 }
